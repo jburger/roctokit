754043bee2b6a533161f1e2aa3d2794a8a07c016
jim <jim@theburge.co>
2019-06-13 21:37:35 +0930

    remove redundant and unauth'd tests The unauth'd tests can be flaky on the build server, because of GH api rate limiting

--- src/tests.rs
+++ src/tests.rs
@@ -4,22 +4,6 @@ use std::env;
 const EXPECTED_ORG_NAME: &str = "silentec";
 
 #[test]
-fn it_can_be_configured_with_token_auth() {
-    get_token_auth_client();
-}
-
-#[test]
-fn it_can_be_configured_with_timeout() {
-    get_unauthenticated_client_with_timeout();
-}
-
-#[test]
-fn it_finds_organizations_anonymously() {
-    let client = get_unauthenticated_client();
-    assert_find_organization(client);
-}
-
-#[test]
 fn it_finds_organizations_token_auth() {
     let client = get_token_auth_client();
     assert_find_organization(client);
@@ -31,13 +15,6 @@ fn assert_find_organization(client: GitHubClient) {
     assert_eq!(organization_result.unwrap().name, EXPECTED_ORG_NAME);
 }
 
-fn get_unauthenticated_client() -> GitHubClient {
-    let mut builder = GitHubClientBuilder::new();
-    builder
-        .for_user_agent(EXPECTED_ORG_NAME)
-        .build()
-}
-
 fn get_token_auth_client() -> GitHubClient {
     let mut builder = GitHubClientBuilder::new();
     let token = env::var("github_token");
@@ -46,13 +23,5 @@ fn get_token_auth_client() -> GitHubClient {
     builder
         .for_user_agent(EXPECTED_ORG_NAME)
         .with_oauth_token(token.unwrap_or_default().as_str())
-        .build()
-}
-
-fn get_unauthenticated_client_with_timeout() -> GitHubClient {
-    let mut builder = GitHubClientBuilder::new();
-    builder
-        .with_timeout(60)
-        .for_user_agent(EXPECTED_ORG_NAME)
         .build()
 }
